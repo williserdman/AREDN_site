ea13098d9b471d74a7c0d50f83056047c01bac50
williserdman <willis.erdman@gmail.com>
2024-06-26 09:13:32 -0700

    Adding mock data to d3

---
+++ src/lib/LinePlot.svelte
@@ -0,0 +1,50 @@
+<script>
+// @ts-nocheck
+
+    import * as d3 from 'd3';
+	import { onMount } from 'svelte';
+  
+    export let data;
+    export let width = 640;
+    export let height = 400;
+    export let marginTop = 20;
+    export let marginRight = 20;
+    export let marginBottom = 20;
+    export let marginLeft = 20;
+    let svg = undefined;
+
+  onMount(() => {
+    const x = d3.scaleLinear()
+      .domain([0, data.length - 1])
+      .range([marginLeft, width - marginRight]);
+
+    const y = d3.scaleLinear()
+      .domain(d3.extent(data))
+      .range([height - marginBottom, marginTop]);
+
+    const line = d3.line()
+      .x((d, i) => x(i))
+      .y(d => y(d));
+
+    const svgEl = d3.select(svg);
+
+    svgEl.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "currentColor")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+
+    svgEl.append("g")
+      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(d3.axisBottom(x));
+
+    svgEl.append("g")
+      .attr("transform", `translate(${marginLeft},0)`)
+      .call(d3.axisLeft(y));
+});
+
+  </script>
+
+
+  <svg width={width} height={height} bind:this={svg}></svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+    import LinePlot from "$lib/LinePlot.svelte"
+	//import "bulma"
+
+    import * as d3 from 'd3';
+
+    let data = d3.ticks(-2, 2, 200).map(Math.sin);
+
+    function onMousemove(event: Event) {
+		const [x, y] = d3.pointer(event);
+    data = data.slice(-200).concat(Math.atan2(x, y));
+  }
+</script>
+
+<div class="x-2">
+	<LinePlot data={data}></LinePlot>
+</div>
