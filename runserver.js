14f16793c840825ec11341fdaaac9bc520039207
williserdman <willis.erdman@gmail.com>
2024-08-16 15:43:55 -0700

    Added support for multiple origins to prevent CORS errors

---
+++ runserver.js
@@ -0,0 +1,26 @@
+import { handler } from "./build/handler.js";
+import polka from "polka";
+import compression from "@polka/compression";
+
+const app = polka();
+
+// https://stackoverflow.com/questions/24897801/enable-access-control-allow-origin-for-multiple-domains-in-node-js
+// function to allow us to have multiple origins (AREDN and reg internet)
+function fixCORSHeaders(req, res, next) {
+	const allowedOrigins = ["http://aredn.williserdman.com", "http://raspberrypi.local.mesh"];
+	const origin = req.headers.origin;
+	if (allowedOrigins.includes(origin)) {
+		res.setHeader("Access-Control-Allow-Origin", origin);
+	}
+
+	res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	res.header("Access-Control-Allow-Credentials", true);
+	return next();
+}
+
+// let SvelteKit handle everything else, including serving prerendered pages and static assets
+app.use(fixCORSHeaders, handler, compression);
+app.listen(3000, () => {
+	console.log("listening on port 3000");
+});
